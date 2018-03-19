e61d35c8097777861fdce76c228c2f50e520db46
Adam Wasylewski <awasylew@users.noreply.github.com>
2018-03-19 05:35:16 +0100

    separate scopes per host iteration

--- p1.js
+++ p1.js
@@ -20,39 +20,37 @@ const output = {fake1:11.1, fake2:null};
 const time = require('moment')().format('YYYYMMDDHHmmss');
 const url = new URL(process.env['STORE_URL']);
 
-for( var host in output ) {
+for( var host in output )
+  (function request_per_host( host ) {
+    const payload = JSON.stringify({
+      origin: "A8-test",
+      target: host,
+      success: output[host] !== null,
+      rtt: output[host],
+      time: time
+    });
+    const options = {
+      protocol: url.protocol,
+      hostname: url.hostname,
+      port: url.port,
+      path: url.pathname,
+      method:'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength( payload )
+      }
+    };
+    var request = http.request( options, http_response );
+    request.on( 'error', (err) => { console.log('[' + host + '] ' + err); });
+    request.write( payload );
+    request.end();
 
-  const payload = JSON.stringify({
-    origin: "A8-test",
-    target: host,
-    success: output[host] !== null,
-    rtt: output[host],
-    time: time
-  });
-  const options = {
-    protocol: url.protocol,
-    hostname: url.hostname,
-    port: url.port,
-    path: url.pathname,
-    method:'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': Buffer.byteLength( payload )
+    function http_response( res ) {
+      res.setEncoding( 'utf8' );
+      res.on( 'data', response_data );
+
+      function response_data(chunk) {
+        console.log('[' + host + '] response: ' + chunk);
+      }
     }
-  };
-  var request = http.request( options, http_response );
-  request.on( 'error', (err) => { console.log('[' + host + '] ' + err); });
-  request.write( payload );
-  request.end();
-
-  function http_response( res ) {
-    res.setEncoding( 'utf8' );
-    res.on( 'data', response_data );
-
-    function response_data(chunk) {
-      console.log(host + ' response: ' + chunk);  //!!! host będzie wartością bieżącą
-    }
-  }
-}
-
-host = 'past';
+  })( host );
